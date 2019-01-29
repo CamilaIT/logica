5e5924050ced1a7e5a0cb34f22af3090232e7e29
php 03 <php.03@zecolmeia.cec.ime.usp.br>
2019-01-29 21:37:58 -0200

    add descricao dos problemas 1 e 2

--- 1A100.php
+++ 1A100.php
@@ -1,5 +1,5 @@
 <?php
-	
+	//Crie um programa que exiba os números de 1 a 100.
 
 	for ($i = 1; $i <= 100; $i++){
 
--- nome100.php
+++ nome100.php
@@ -1,4 +1,6 @@
 <?php
+	//Crie um programa que exiba o seu nome 100 vezes.
+	
 	$nome = Camila;
 
 	for ($i = 1; $i <= 100; $i++){
