b6fbfc9aa2c553cb0d9dd6a0956299003ca3427b
Camila <camila0830@gmail.com>
2019-01-30 14:39:55 -0200

    identação

--- varios_triangulos.php
+++ varios_triangulos.php
@@ -5,17 +5,16 @@
 
 	$x = mt_rand(3, 25);
 
-		echo "Total de triangulos: $x".PHP_EOL;
+	echo "Total de triangulos: $x".PHP_EOL;
 
 	for ($n=1; $n <= $x ; $n++) {
 
-		echo "$n".PHP_EOL;
+			echo "$n".PHP_EOL;
 
 		for ($i=1; $i <=5 ; $i++) { 
 
-		echo str_pad("*", $i, "*").PHP_EOL;
+			echo str_pad("*", $i, "*").PHP_EOL;
+		}
 	}
-
-}
 
 ?>
